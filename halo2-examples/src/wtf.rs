b718bf2074c2e004787148430d7443b97fe2a1e9
jgorzny <j.gorzny@gmail.com>
2022-12-07 15:32:33 +0100

    initial model iteration

--- halo2-examples/src/wtf.rs
+++ halo2-examples/src/wtf.rs
@@ -28,8 +28,8 @@ fn main() {
     public_input[2] += Fp::one();
     let _prover = MockProver::run(k, &circuit, vec![public_input]).unwrap();
 
-    exampleZ3();
-
+    //exampleZ3();
+    testCountModels();
 }
 
 fn exampleZ3() {
@@ -101,4 +101,77 @@ fn exampleZ3() {
     assert!(yv % 7 == 2);
     assert!(xv + 2 > 7);
     */
+}
+
+fn testCountModels() {
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+
+    let a = ast::Int::new_const(&ctx, "a");
+    let b = ast::Int::new_const(&ctx, "b");
+    let h = ast::Int::new_const(&ctx, "h");
+    let o = ast::Int::new_const(&ctx, "o");
+    let zero = ast::Int::from_i64(&ctx, 0);
+    let one = ast::Int::from_i64(&ctx, 1);
+
+    let solver = Solver::new(&ctx);
+
+
+   //    h*(a+b)+(1-h)(a*b) - o  = 0
+
+    // solver.assert(&x.gt(&y)); // old; for reference
+
+    let firstTerm = ast::Int::mul(&ctx, &[&h, &ast::Int::add(&ctx, &[&a, &b])]);
+    let secondTerm = ast::Int::mul(&ctx, &[&ast::Int::sub(&ctx, &[&one, &h]), &ast::Int::add(&ctx, &[&a, &b])]);
+    let formulaSum = ast::Int::add(&ctx, &[&firstTerm, &secondTerm]);
+    let formula = ast::Int::sub(&ctx, &[&formulaSum, &o]);
+    let finalCount = countModels(&ctx, formula._eq(&zero), vec![a,b,h,o]);
+    println!("Final count: {}", finalCount);
+}
+
+fn countModels(ctx: &z3::Context,formula: z3::ast::Bool, varsList: Vec<z3::ast::Int>) -> i32 {
+    let mut count = 0;
+
+
+    let solver = Solver::new(&ctx);
+
+    solver.assert(&formula);
+    println!("Going to check... {}", &formula);
+
+    loop {
+         if count > 1 {
+             break 0;
+         }
+
+         if solver.check() != SatResult::Sat {
+             break 0;
+         }
+
+         assert_eq!(solver.check(), SatResult::Sat);
+         println!("Result is SAT");
+         let model = solver.get_model().unwrap();
+         println!("model: \n{}", model);
+
+         // testing only; not needed due to previous line in production.
+         /*
+         for var in varsList.iter() {
+             let v = model.eval(var, true).unwrap().as_i64().unwrap();
+             println!("{} -> {}", var, v);
+         }
+         */
+
+
+         // todo: this assumes *every* variables has a different value in the new model; this is too restrictive
+         for var in varsList.iter() {
+             let v = model.eval(var, true).unwrap().as_i64().unwrap();
+             solver.assert(
+                    &z3::ast::Bool::<'_>::or(&ctx,
+                        &[&var.gt( &ast::Int::from_i64(ctx, v)), &var.lt( &ast::Int::from_i64(ctx, v))]
+                    ));
+            println!("{} -> {}", var, v);
+         }
+
+         count = count + 1;
+    }
+
 }
